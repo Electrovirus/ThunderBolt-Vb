1a511a2daeaf4d6347a2fbe636268cb8faed4c7a
Electrovirus <E.electrovirus@gmail.com>
2017-01-20 19:22:41 +0330

    TB | tg-cli

--- tg/tdcli.lua
+++ tg/tdcli.lua
@@ -1,20 +1,4 @@
---[[
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-   MA 02110-1301, USA.
-
-]]--
 
 -- Vector example form is like this: {[0] = v} or {v1, v2, v3, [0] = v}
 -- If false or true crashed your telegram-cli, try to change true to 1 and false to 0
